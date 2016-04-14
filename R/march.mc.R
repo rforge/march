e0903973e9b39e3384e5966f3f063d7abd397b55
oliverbuschor <oliverbuschor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-14 08:42:13 +0000

    commented out the march.mc.bailey function, it isn't used yet
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/march@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/march.mc.R
+++ R/march.mc.R
@@ -1,17 +1,17 @@
-# 
+#
 # Author: ogiermaitre
 ###############################################################################
 
 #' Construct an homogeneous Markov Chain.
-#' 
-#' A \code{\link{march.Mc}} object of order \emph{order} is constructed from 
+#'
+#' A \code{\link{march.Mc}} object of order \emph{order} is constructed from
 #' the dataset \emph{y}. The first maxOrder-order elements of each sequence of the dataset are truncated in order to return a model
-#' which can be compared with other Markovian models of visible order maxOrder. 
-#' 
+#' which can be compared with other Markovian models of visible order maxOrder.
+#'
 #' @param y the \code{\link{march.Dataset}} from which the homogeneous Markov chain will be constructed.
 #' @param order the order of the constructed Markov Chain.
 #' @param maxOrder the maximum visible order among the set of Markovian models to compare.
-#' 
+#'
 #' @return the \code{\link{march.Mc}} of order \emph{order} constructed \emph{w.r.t} the dataset \emph{y} and maxOrder.
 #'
 #' @author Ogier Maitre
@@ -19,14 +19,14 @@
 #' @seealso \code{\link{march.Mc-class}}, \code{\link{march.Model-class}}, \code{\link{march.Dataset-class}}.
 #' @export
 march.mc.construct <- function(y,order,maxOrder=order) {
-  
+
   order <- march.h.paramAsInteger(order)
   maxOrder <- march.h.paramAsInteger(maxOrder)
-  
+
   if( order>maxOrder ){
     stop("maxOrder should be greater or equal than order")
   }
-  	
+
 	rt <- array(0,c(y@K^order,y@K))
 	for(i in 1:y@N){
 		if(y@T[i]>order){
@@ -43,7 +43,7 @@ march.mc.construct <- function(y,order,maxOrder=order) {
 			}
 		}
 	}
-	
+
 	tot <- rt%*%array(1,c(y@K,1))
 	r <- array(0,c(y@K^order,y@K))
 	for(i in 1:y@K^order){
@@ -51,7 +51,7 @@ march.mc.construct <- function(y,order,maxOrder=order) {
 			r[i,] <- rt[i,]/tot[i]
 		}
 	}
-	
+
 	ll <- 0
 	for(i in 1:(y@K^order)){
 		for(j in 1:y@K){
@@ -60,29 +60,29 @@ march.mc.construct <- function(y,order,maxOrder=order) {
 			}
 		}
 	}
-  
+
 	new("march.Mc",RC=r,order=order,ll=ll,RT=rt,y=y,dsL=sum(rt),nbZeros=length(which(rt==0)))
 }
 
 
-march.mc.bailey <- function( indep, y, alpha ){
-	
-	n <- sum(y@T)
-	n_i <- rowSums(mc@RT)
-
-	A <- march.ci.h.A(n,n_i)
-	B <- march.ci.h.B(n,n_i)	
-	C <- march.ci.h.C(alpha,y@K,n)
-	
-	pm <- array(NA,c(length(n_i)))
-	pp <- array(NA,c(length(n_i)))
-	
-	for( i in 1:length(n_i)){
-		if( n_i[i]>0 ){
-			pm[i] <- (sqrt(A[i])-sqrt(C*(C+1-A[i])))^2/(C+1)^2
-			pp[i] <- (sqrt(B[i])+sqrt(C*(C+1-B[i])))^2/(C+1)^2
-		}
-	}
-
-	list(pm=pm,pp=pp)
-}
+#march.mc.bailey <- function( indep, y, alpha ){
+#
+#	n <- sum(y@T)
+#	n_i <- rowSums(mc@RT)
+#
+#	A <- march.ci.h.A(n,n_i)
+#	B <- march.ci.h.B(n,n_i)
+#	C <- march.ci.h.C(alpha,y@K,n)
+#
+#	pm <- array(NA,c(length(n_i)))
+#	pp <- array(NA,c(length(n_i)))
+#
+#	for( i in 1:length(n_i)){
+#		if( n_i[i]>0 ){
+#			pm[i] <- (sqrt(A[i])-sqrt(C*(C+1-A[i])))^2/(C+1)^2
+#			pp[i] <- (sqrt(B[i])+sqrt(C*(C+1-B[i])))^2/(C+1)^2
+#		}
+#	}
+#
+#	list(pm=pm,pp=pp)
+#}
