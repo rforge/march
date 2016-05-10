560d7186e6227b83da24a4f6f977535e9f516195
andreberchtold <andreberchtold@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-10 22:49:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/march@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/march.model.R
+++ pkg/R/march.model.R
@@ -6,7 +6,7 @@
 
 
 #
-# As march.Model is an abstract class, this code should never be called
+# A march.model is an abstract class, this code should never be called
 #
 march.model.name <- function(object){
 	c("Model")
