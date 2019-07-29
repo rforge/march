8fdd7ff919ef89f3a2bbc45e10ba439654adf92a
kemery <kemery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-29 12:18:59 +0000

    Longest examples not ran anymore
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/march@108 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/examples/march.dcmm.viterbi.example.R
+++ pkg/tests/examples/march.dcmm.viterbi.example.R
@@ -1,6 +1,6 @@
 set.seed(327)
 # Computation of a DCMM model
-model <- march.dcmm.construct(y=pewee,orderHC=1,orderVC=1,M=2,popSize=1,gen=1)
+\dontrun{model <- march.dcmm.construct(y=pewee,orderHC=1,orderVC=1,M=2,popSize=1,gen=1)
 # Extraction of the best sequence of hidden states using the Viterbi algorithm.
 bestSeq <- march.dcmm.viterbi(model)
-print(bestSeq)
+print(bestSeq)}
